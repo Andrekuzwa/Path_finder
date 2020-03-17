e544ac9bce1094cf4196484f31ab9855104cd0ff
Andrekuzwa <48141764+Andrekuzwa@users.noreply.github.com>
2020-03-17 20:28:25 +0100

    endNode condition TODO

--- finder.py
+++ finder.py
@@ -126,9 +126,55 @@ def aStar(grid,start,end):
     closed = []
 
     startNode = Node(None,start)
+    startNode.f,startNode.h,startNode.g = 0,0,0
     endNode = Node(None,end)
+    endNode.f,endNode.h,endNode,g = 0,0,0
 
     open.append(startNode)
+
+    children_vectors = ((-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0))
+
+    while len(open) > 0:
+
+        current_node = open[0]
+        current_index = 0
+
+        #finding node with best f value
+        for index,item in enumerate(open):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        #remove best node from open and add to closed list
+        open.pop(current_index)
+        closed.append(current_node)
+
+        children = []
+        for j in children_vectors:
+            if (current_node.position[0]+j[0]) > 0 and (current_node.position[0]+j[0]) < 50 and (current_node.position[1]+j[1]) > 0 and (current_node.position[1]+j[1]) < 50:
+                if grid[current_node.position[0]+j[0]][current_node.position[1]+j[1]] != 1 and Node(current_node,(current_node.position[0]+j[0],current_node.position[1]+j[1])) not in closed:
+                    children.append(Node(current_node,(current_node.position[0]+j[0],current_node.position[1]+j[1])))
+                    if (j[0]**2 + j[1]**2)>1:
+                        children[-1].g = children[-1].parent.g + math.sqrt(2)
+                    else:
+                        children[-1].g = children[-1].parent.g + 1
+                    children[-1].h = math.sqrt((children[-1].position[0]-endNode.position[0])**2 + (children[-1].position[1]-endNode.position[1])**2)
+                    children[-1].f = children[-1].h + children[-1].g
+
+                    flag = False
+                    for open_item in open:
+                        if children[-1] == open_item and children[-1].g > open_item.g:
+                            flag = True
+                    if flag == False:
+                        open.append(children[-1])
+
+
+        #here if found ->later
+
+
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -15,3 +15,18 @@ print(c.beka,c.meh)
 print(k==c)
 
 
+lista = [XD(10,20) for i in range(10)]
+print(lista)
+print((0,0) + (1,1))
+
+lista1 = [7]
+lista = lista1
+print(lista)
+print([1,2] in lista1)
+
+for i in [j for j in range(10)]:
+    print(i)
+    if i in lista1:
+        continue
+print('elo')
+
