ad9f5cf3cdc3b3c779d1d28ac99bb718001c67c8
Andrekuzwa <48141764+Andrekuzwa@users.noreply.github.com>
2020-03-18 13:12:54 +0100

    Only path

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import math
 class XD:
     def __init__(self,beka,meh):
         self.beka = beka
@@ -6,27 +7,81 @@ class XD:
     def __eq__(self, other):
         return self.beka == other.beka
 
-k = XD(10,20)
-c = XD(10,30)
+class Node:
+    def __init__(self,parent = None,position = None):
+        self.parent = parent
+        self.position = position
 
-print(k.beka,k.meh)
-print(c.beka,c.meh)
+        self.f = 0
+        self.g = 0
+        self.h = 0
 
-print(k==c)
 
 
-lista = [XD(10,20) for i in range(10)]
-print(lista)
-print((0,0) + (1,1))
 
-lista1 = [7]
-lista = lista1
-print(lista)
-print([1,2] in lista1)
+def aStar(grid,start,end):
 
-for i in [j for j in range(10)]:
-    print(i)
-    if i in lista1:
-        continue
-print('elo')
+    open = []
+    closed = []
 
+    startNode = Node(None,start)
+    startNode.f,startNode.h,startNode.g = 0,0,0
+    endNode = Node(None,end)
+    endNode.f,endNode.h,endNode.g = 0,0,0
+
+    open.append(startNode)
+
+    children_vectors = ((-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0))
+
+    while len(open) > 0:
+
+        current_node = open[0]
+        current_index = 0
+
+        #finding node with best f value
+        for index,item in enumerate(open):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        #remove best node from open and add to closed list
+        open.pop(current_index)
+        closed.append(current_node)
+
+        if current_node.position == endNode.position:
+            path = []
+            current = current_node
+            while current.parent != None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1]
+
+        children = []
+        for j in children_vectors:
+            if (current_node.position[0]+j[0]) > 0 and (current_node.position[0]+j[0]) < 50 and (current_node.position[1]+j[1]) > 0 and (current_node.position[1]+j[1]) < 50:
+                if grid[current_node.position[0]+j[0]][current_node.position[1]+j[1]] != 1 and Node(current_node,(current_node.position[0]+j[0],current_node.position[1]+j[1])) not in closed:
+                    children.append(Node(current_node,(current_node.position[0]+j[0],current_node.position[1]+j[1])))
+                    if (j[0]**2 + j[1]**2)>1:
+                        children[-1].g = children[-1].parent.g + math.sqrt(2)
+                    else:
+                        children[-1].g = children[-1].parent.g + 1
+                    children[-1].h = math.sqrt((children[-1].position[0]-endNode.position[0])**2 + (children[-1].position[1]-endNode.position[1])**2)
+                    children[-1].f = children[-1].h + children[-1].g
+
+                    flag = False
+                    for open_item in open:
+                        if children[-1] == open_item and children[-1].g > open_item.g:
+                            flag = True
+                    if flag == False:
+                        open.append(children[-1])
+
+
+grid = []
+for row in range(50):
+    # Add an empty array that will hold each cell
+    # in this row
+    grid.append([])
+    for column in range(50):
+        grid[row].append(0)  # Append a cell
+
+print(aStar(grid,(0,0),(10,15)))
