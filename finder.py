874a6fb1932d5abbb77d5616a887d92ebdc572a2
Andrekuzwa <48141764+Andrekuzwa@users.noreply.github.com>
2020-03-18 13:51:26 +0100

    colored after calculation

--- finder.py
+++ finder.py
@@ -6,7 +6,7 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-
+BLUE = (0,0,255)
 # This sets the WIDTH and HEIGHT of each grid location
 WIDTH = 12
 HEIGHT = 12
@@ -57,6 +57,14 @@ def aStar(grid,start,end):
     children_vectors = ((-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0))
 
     while len(open) > 0:
+
+        for i in open:
+            if i.position is not None:
+                grid[i.position[0]][i.position[1]] = 4
+
+        for i in closed:
+            if i.position is not None:
+                grid[i.position[0]][i.position[1]] = 3
 
         current_node = open[0]
         current_index = 0
@@ -115,7 +123,7 @@ done = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-# -------- Main Program Loop -----------
+
 while not done:
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
@@ -166,7 +174,11 @@ while not done:
             if grid[row][column] == 1:
                 color = WHITE
             elif grid[row][column] == 2:
+                color = BLUE
+            elif grid[row][column] == 3:
                 color = RED
+            elif grid[row][column] == 4:
+                color = GREEN
             pygame.draw.rect(screen,
                              color,
                              [(MARGIN + WIDTH) * column + MARGIN,
@@ -185,75 +197,6 @@ while not done:
 pygame.quit()
 
 
-
-class Node:
-    def __init__(self,parent = None,position = None):
-        self.parent = parent
-        self.position = position
-
-        self.f = 0
-        self.g = 0
-        self.h = 0
-
-    def __eq__(self, other):
-        return self.postion == other.position
-
-
-def aStar(grid,start,end):
-
-    open = []
-    closed = []
-
-    startNode = Node(None,start)
-    startNode.f,startNode.h,startNode.g = 0,0,0
-    endNode = Node(None,end)
-    endNode.f,endNode.h,endNode,g = 0,0,0
-
-    open.append(startNode)
-
-    children_vectors = ((-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0))
-
-    while len(open) > 0:
-
-        current_node = open[0]
-        current_index = 0
-
-        #finding node with best f value
-        for index,item in enumerate(open):
-            if item.f < current_node.f:
-                current_node = item
-                current_index = index
-
-        #remove best node from open and add to closed list
-        open.pop(current_index)
-        closed.append(current_node)
-
-        if current_node.position == endNode.position:
-            path = []
-            current = current_node
-            while current.parent != None:
-                path.append(current.position)
-                current = current.parent
-            return path[::-1]
-
-        children = []
-        for j in children_vectors:
-            if (current_node.position[0]+j[0]) > 0 and (current_node.position[0]+j[0]) < 50 and (current_node.position[1]+j[1]) > 0 and (current_node.position[1]+j[1]) < 50:
-                if grid[current_node.position[0]+j[0]][current_node.position[1]+j[1]] != 1 and Node(current_node,(current_node.position[0]+j[0],current_node.position[1]+j[1])) not in closed:
-                    children.append(Node(current_node,(current_node.position[0]+j[0],current_node.position[1]+j[1])))
-                    if (j[0]**2 + j[1]**2)>1:
-                        children[-1].g = children[-1].parent.g + math.sqrt(2)
-                    else:
-                        children[-1].g = children[-1].parent.g + 1
-                    children[-1].h = math.sqrt((children[-1].position[0]-endNode.position[0])**2 + (children[-1].position[1]-endNode.position[1])**2)
-                    children[-1].f = children[-1].h + children[-1].g
-
-                    flag = False
-                    for open_item in open:
-                        if children[-1] == open_item and children[-1].g > open_item.g:
-                            flag = True
-                    if flag == False:
-                        open.append(children[-1])
 
 
 
