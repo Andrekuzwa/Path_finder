33231c906181618becb82aee9371ec9607ad7750
Andrekuzwa <48141764+Andrekuzwa@users.noreply.github.com>
2020-03-19 02:21:53 +0100

    debug time

--- finder.py
+++ finder.py
@@ -7,6 +7,7 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0,0,255)
+SMTH = (100,0,100)
 # This sets the WIDTH and HEIGHT of each grid location
 WIDTH = 12
 HEIGHT = 12
@@ -27,8 +28,49 @@ for row in range(50):
 # for i in [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (6, 7), (7, 8), (7, 9), (8, 10), (8, 11), (9, 12), (9, 13), (9, 14), (10, 15)]:
 #     grid[i[0]][i[1]] = 1
 
+
+# Initialize pygame
+pygame.init()
+
+
+# Set the HEIGHT and WIDTH of the screen
+WINDOW_SIZE = [651, 651]
+screen = pygame.display.set_mode(WINDOW_SIZE)
+
+# Set title of screen
+pygame.display.set_caption("Finder")
+
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
 start_test = (1,1)
-end_test = (10,10)
+end_test = (20,20)
+
+# Draw the grid
+def draw(grid):
+    for row in range(50):
+        for column in range(50):
+            color = BLACK
+            if grid[row][column] == 1:
+                color = WHITE
+            elif grid[row][column] == 2:
+                color = BLUE
+            elif grid[row][column] == 3:
+                color = RED
+            elif grid[row][column] == 4:
+                color = GREEN
+            elif grid[row][column] == 5:
+                color = SMTH
+            pygame.draw.rect(screen,
+                             color,
+                             [(MARGIN + WIDTH) * column + MARGIN,
+                              (MARGIN + HEIGHT) * row + MARGIN,
+                              WIDTH,
+                              HEIGHT])
 
 class Node:
     def __init__(self,parent = None,position = None):
@@ -58,13 +100,24 @@ def aStar(grid,start,end):
 
     while len(open) > 0:
 
+        draw(grid)
+        pygame.display.flip()
+        print("OPEN-------->",len(open))
+        print(len(closed))
+
         for i in open:
             if i.position is not None:
-                grid[i.position[0]][i.position[1]] = 4
+                if grid[i.position[0]][i.position[1]] == 4:
+                    grid[i.position[0]][i.position[1]] = 5
+                else:
+                    grid[i.position[0]][i.position[1]] = 4
 
         for i in closed:
             if i.position is not None:
-                grid[i.position[0]][i.position[1]] = 3
+                # if grid[i.position[0]][i.position[1]] == 3:
+                #     grid[i.position[0]][i.position[1]] = 5
+                # else:
+                    grid[i.position[0]][i.position[1]] = 3
 
         current_node = open[0]
         current_index = 0
@@ -89,7 +142,7 @@ def aStar(grid,start,end):
 
         children = []
         for j in children_vectors:
-            if (current_node.position[0]+j[0]) > 0 and (current_node.position[0]+j[0]) < 50 and (current_node.position[1]+j[1]) > 0 and (current_node.position[1]+j[1]) < 50:
+            if (current_node.position[0]+j[0]) >= 0 and (current_node.position[0]+j[0]) < 50 and (current_node.position[1]+j[1]) >= 0 and (current_node.position[1]+j[1]) < 50:
                 if grid[current_node.position[0]+j[0]][current_node.position[1]+j[1]] != 1 and Node(current_node,(current_node.position[0]+j[0],current_node.position[1]+j[1])) not in closed:
                     children.append(Node(current_node,(current_node.position[0]+j[0],current_node.position[1]+j[1])))
                     if (j[0]**2 + j[1]**2)>1:
@@ -101,27 +154,11 @@ def aStar(grid,start,end):
 
                     flag = False
                     for open_item in open:
-                        if children[-1] == open_item and children[-1].g > open_item.g:
+                        if children[-1] == open_item:
                             flag = True
                     if flag == False:
                         open.append(children[-1])
 
-
-# Initialize pygame
-pygame.init()
-
-# Set the HEIGHT and WIDTH of the screen
-WINDOW_SIZE = [651, 651]
-screen = pygame.display.set_mode(WINDOW_SIZE)
-
-# Set title of screen
-pygame.display.set_caption("Finder")
-
-# Loop until the user clicks the close button.
-done = False
-
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
 
 
 while not done:
@@ -134,6 +171,9 @@ while not done:
                 path = aStar(grid,start_test,end_test)
                 for i in path:
                     grid[i[0]][i[1]] = 2
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
 
         #Drawing with left click
         if pygame.mouse.get_pressed()[0]:
@@ -167,24 +207,8 @@ while not done:
     # Set the screen background
     screen.fill(WHITE)
 
-    # Draw the grid
-    for row in range(50):
-        for column in range(50):
-            color = BLACK
-            if grid[row][column] == 1:
-                color = WHITE
-            elif grid[row][column] == 2:
-                color = BLUE
-            elif grid[row][column] == 3:
-                color = RED
-            elif grid[row][column] == 4:
-                color = GREEN
-            pygame.draw.rect(screen,
-                             color,
-                             [(MARGIN + WIDTH) * column + MARGIN,
-                              (MARGIN + HEIGHT) * row + MARGIN,
-                              WIDTH,
-                              HEIGHT])
+
+    draw(grid)
 
     # Limit to 60 frames per second
     clock.tick(60)
